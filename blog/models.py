079064ed00ff8715a468c0c1cf02db6eae6d65d4
Maxim Kalita <kalitamaxwork@gmail.com>
2020-09-03 14:28:27 +0300

    Added blog templates.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create a custom Model Manager
@@ -15,6 +16,7 @@ class Post(models.Model):
         ('published', 'Published'),
     )
     title = models.CharField(max_length=250)
+    # Can contain characters, numbers, hyphens, and underscores.
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
@@ -28,7 +30,10 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
-    
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month,
+                        self.publish.day, self.slug])
     
     class Meta:
         ordering = ('-publish', )
